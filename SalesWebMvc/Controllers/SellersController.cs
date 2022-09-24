dbd178dd3751102e979db614b45da4c84ea38984
Wanderley Pereira <wanderleyps794@gmail.com>
2022-09-24 16:21:13 -0300

    Simple create form

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
@@ -16,6 +17,18 @@ namespace SalesWebMvc.Controllers
             var list = _sellerService.FindAll();
             //passando a lista para a view para ser gerada no carregamento
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
