3ea3330c7b584959973b598646aba67ceb9157d4
Wanderley Pereira <wanderleyps794@gmail.com>
2022-10-12 17:18:17 -0300

    Exception handling for delete (referential integrity)

---
+++ SalesWebMvc/Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Services.Exceptions
+{
+    /*
+     * Exceçãoa personalizada de serviço para erros de integridade referencial, 
+     * ou seja quando se quer deletar entidades que possuem dependência com outras
+     * Exemplo vendedor e vendas.
+     */
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException(string message) : base(message)
+        {
+        }
+    }
+}
